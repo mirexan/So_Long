7e999b19553249c86d83d6ee7c03da1f2619606a
Mirexan <mmregada@gmail.com>
2025-05-17 13:59:37 +0200

    Añado impresion del mapa recibido, asi como mensajes de error

--- main_so_long.c
+++ main_so_long.c
@@ -1,7 +1,41 @@
 #include "so_long.h"
 
-int	main()
+int	main(int argc, char *argv[])
 {
-  ft_printf("Hola que ase\n");
+  int   fd;
+  char  **map;
+
+  if (argc != 2)
+  {
+    ft_putstr_fd("Error, no se ha adjuntado mapa\n", 2);
+    return (1);
+  }
+  fd = open(argv[1], O_RDONLY);
+  if (fd < 0)
+  {
+    ft_putstr_fd("Error, no se ha podido abrir el mapa\n",2);
+    return (1);
+  }
+  map = read_maps(fd);
+  if (!map || !check_map(map))
+  {
+    ft_putstr_fd("Error mapa\n", 2);
+    free_map(map);
+    return(1);
+  }
+  int i = 0;
+  int j;
+  while (map[i])
+  {
+    j = 0;
+    while (map[i][j])
+    {
+      ft_printf("%c", map[i][j]);
+      j++;
+    }
+    ft_printf("\n");
+    i++;
+  }
+  close(fd);
   return (0);
 }
